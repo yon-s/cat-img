5b20a02cc7bac2ca8778b162f9fb1270397c20b3
yonehara <shuichi.yonehara@gmail.com>
2023-11-07 15:42:14 +0900

    [add]PWA対応のコード追加 [理由]PWAに対応するため

--- next.config.js
+++ next.config.js
@@ -2,3 +2,15 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  //開発中にのみ発生するエラーが出ないように
+  disable: process.env.NODE_ENV === "development",
+})
+
+module.exports = withPWA({
+  //next.js config
+  reactStrictMode: true,
+})
